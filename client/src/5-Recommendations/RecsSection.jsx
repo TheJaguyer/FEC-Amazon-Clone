0f40ea1b597b4810ababd40cc21158777dd9be28
roymansoor <roy.mansoor@gmail.com>
2022-12-21 13:05:46 -0600

    formated the codes

--- client/src/5-Recommendations/RecsSection.jsx
+++ client/src/5-Recommendations/RecsSection.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useRef } from 'react';
-import './recs.css';
-import { AiOutlineLeft, AiOutlineRight, TbDotsVertical } from 'react-icons/all';
-
-import RecsItem from './RecsItem';
+import React, { useState, useRef } from "react";
+import "./recs.css";
+import { AiOutlineLeft, AiOutlineRight, TbDotsVertical } from "react-icons/all";
+import RecsItem from "./RecsItem";
 
 const RecsSection = () => {
   return (
@@ -10,7 +9,7 @@ const RecsSection = () => {
       <div className="recs-section-big">
         <div className="recs-section">
           <div className="head">
-            {' '}
+            {" "}
             <h2 className="head">More items to explore</h2>
             <p className="head-right">page 1 of 1</p>
             <TbDotsVertical className="head-right1" size="1.5em" />
@@ -18,14 +17,14 @@ const RecsSection = () => {
           <div className="btn-sections">
             <div className="left-side">
               <div className="btn-next-right">
-                {' '}
+                {" "}
                 <span className="arrow-btn-outer">
-                  {' '}
+                  {" "}
                   <i className="arrow-btn">
-                    {' '}
+                    {" "}
                     <AiOutlineLeft size="1em" />
                   </i>
-                </span>{' '}
+                </span>{" "}
               </div>
             </div>
             <div className="items">
@@ -33,14 +32,14 @@ const RecsSection = () => {
             </div>
             <div className="right-side">
               <div className="btn-next-left">
-                {' '}
+                {" "}
                 <span className="arrow-btn-outer">
-                  {' '}
+                  {" "}
                   <i className="arrow-btn">
-                    {' '}
+                    {" "}
                     <AiOutlineRight size="1em" />
                   </i>
-                </span>{' '}
+                </span>{" "}
               </div>
             </div>
           </div>
