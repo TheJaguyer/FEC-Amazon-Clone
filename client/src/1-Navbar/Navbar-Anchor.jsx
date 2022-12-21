3bda12d1d2e55a3632f074731b3358c1b0e4400e
Jarrett Guyer <guyer.jarrett@gmail.com>
2022-12-21 08:08:48 -0700

    added some styling to nav anchor

--- client/src/1-Navbar/Navbar-Anchor.jsx
+++ client/src/1-Navbar/Navbar-Anchor.jsx
@@ -1,32 +1,44 @@
-import Overlay1 from "./Overlay"
+import Overlay from './Overlay';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import CountDown from "./Countdown";
-import './Navbar.module.css'
-function NavAnchor () {
-    return (
-        <div style={{backgroundColor:'#232f3e', color:"white", alignItems:'center', justifyContent:"space-between", display:"flex", fontSize: '14px', lineHeight:'12px'}}>
-            <Overlay1 />
-            <span>Very Merry Deals</span>
-            <span>Holiday Gift Guide</span>
-            <span>Best Sellers</span>
-            <span>Amazon Basics</span>
-            <span>Customer Service</span>
-            <span>Prime</span>
-            <span>New Releases</span>
-            <span>Books</span>
-            <span>Music</span>
-            <span>Registry</span>
-            <span>Gift Cards</span>
-            <span>Amazon Home</span>
-            <span>Fashion</span>
-            <div className="countdown-gif">
-            <img src="/Navbar-images/countdownClock.gif"/>
-            <CountDown />
-            </div>
-        </div>
+import CountDown from './CountDown.jsx';
+import './Navbar.module.css';
+function NavAnchor() {
+  return (
+    <div
+      style={{
+        backgroundColor: '#232f3e',
+        color: 'white',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        display: 'flex',
+        flexFlow: 'row nowrap',
+        overflow: 'hidden',
+        fontSize: '14px',
+        lineHeight: '12px',
+        whiteSpace: 'nowrap',
+      }}
+    >
+      <Overlay />
+      <span>Very Merry Deals</span>
+      <span>Holiday Gift Guide</span>
+      <span>Best Sellers</span>
+      <span>Amazon Basics</span>
+      <span>Customer Service</span>
+      <span>Prime</span>
+      <span>New Releases</span>
+      {/* <span>Books</span>
+      <span>Music</span>
+      <span>Registry</span>
+      <span>Gift Cards</span>
+      <span>Amazon Home</span>
+      <span>Fashion</span> */}
 
-
-
-    )
+      {/* I will find a way to make excess items disappear like how they do on the real site */}
+      <div className="countdown-gif" style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
+        <img src="/Navbar-images/countdownClock.gif" style={{ gridColumn: '1 / span 2', gridRow: '1 / span 2' }} />
+        <CountDown />
+      </div>
+    </div>
+  );
 }
-export default NavAnchor
+export default NavAnchor;
